1d4c547d46f7e13cc4c66e2f30fd8f092e45845c
Vishal Prajapati <vishalp951295@gmail.com>
2024-09-05 11:27:55 +0530

    Add project files.

---
+++ Try Catch Finally/ErrorExcaption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch_Finally
+{
+    public class ErrorExcaption : Exception
+    {
+        public ErrorExcaption(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Try Catch Finally/Program.cs
@@ -0,0 +1,18 @@
+﻿using Try_Catch_Finally;
+
+try
+{
+    int country = Convert.ToInt32(Console.ReadLine());
+    if (country >191)
+    {
+        throw new ErrorExcaption("error");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("finally");
+}
